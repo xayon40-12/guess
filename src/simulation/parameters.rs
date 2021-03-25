b926e98e5df7401793750f894e4ff3e9864497bf
xayon40-12 <touroux.nathan@gmail.com>
2021-03-25 09:54:05 +0100

    add symetric dim

--- src/simulation/parameters.rs
+++ src/simulation/parameters.rs
@@ -75,7 +75,9 @@ impl Noises {
 pub enum DimPhy {
     D1((usize, f64)),
     D2((usize, f64), (usize, f64)),
+    D2S((usize, f64)),
     D3((usize, f64), (usize, f64), (usize, f64)),
+    D3S((usize, f64)),
 }
 
 impl From<DimPhy> for ([usize; 3], [f64; 3]) {
@@ -83,7 +85,9 @@ impl From<DimPhy> for ([usize; 3], [f64; 3]) {
         match d {
             DimPhy::D1((u, f)) => ([u, 1, 1], [f, 0.0, 0.0]),
             DimPhy::D2((u0, f0), (u1, f1)) => ([u0, u1, 1], [f0, f1, 0.0]),
+            DimPhy::D2S((u, f)) => ([u, u, u], [f, f, f]),
             DimPhy::D3((u0, f0), (u1, f1), (u2, f2)) => ([u0, u1, u2], [f0, f1, f2]),
+            DimPhy::D3S((u, f)) => ([u, u, u], [f, f, f]),
         }
     }
 }
