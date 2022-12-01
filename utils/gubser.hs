20ffc8120ae83fadd4d3b717a41dee2026a3c09f
xayon40-12 <touroux.nathan@gmail.com>
2022-12-01 14:47:22 +0900

    use python instead of haskell

--- utils/gubser.hs
+++
@@ -1,5 +0,0 @@
-f [x,y,t] = 2**(8/3)/(t**(4/3)*(1+2*(t**2+r**2)+(t**2-r**2)**2)**(4/3))
-    where r = sqrt (x**2 + y**2)
-
-main :: IO ()
-main = getContents >>= mapM_ (print . f . map read . words) . lines
